990a8f0a0ccbcfae348a143aa40d0c05190847c7
Justin Moy <justincmoy@users.noreply.github.com>
2021-07-08 14:53:46 -0600

    Add mouse forward and back buttons

--- keyboards/keyboardio/atreus/keymaps/justincmoy/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/justincmoy/keymap.c
@@ -205,7 +205,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_BSPC, KC_DEL,  U_NP,    U_NP
   ),
   [MOUSE] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    KC_BTN4, KC_BTN5, U_NU,    U_NU,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
